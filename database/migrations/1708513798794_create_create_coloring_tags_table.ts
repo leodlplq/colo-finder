2d86828e02cb372f6265f455b7ea599150701788
Léo Delplanque <delplanqueleomail@gmail.com>
2024-02-21 15:50:31 +0100

    fix(tag): fix relation on tag_coloring

--- database/migrations/1708513798794_create_create_coloring_tags_table.ts
+++ database/migrations/1708513798794_create_create_coloring_tags_table.ts
@@ -7,8 +7,8 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.integer('tag_id').unsigned().references('tags.id').notNullable()
-      table.integer('book_id').unsigned().references('books.id').notNullable()
-      table.unique(['tag_id', 'book_id'])
+      table.integer('coloring_id').unsigned().references('colorings.id').notNullable()
+      table.unique(['tag_id', 'coloring_id'])
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
